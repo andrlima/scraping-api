a30aa17756cbb18fb85eb3a9aa61eeaba5f03f40
andrlima <tc.leo.araujo@gmail.com>
2021-01-09 21:28:26 -0300

    Conectando
    Conectando com o JSoup no google

--- src/main/java/br/com/exemplo/dto/PartidaGoogleDTO.java
+++ src/main/java/br/com/exemplo/dto/PartidaGoogleDTO.java
@@ -2,17 +2,19 @@ package br.com.exemplo.dto;
 
 import java.io.Serializable;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 // DTO = Data Transfer Object
 
-//O lombok esta criaqndo os gatters, setters e o método construtor, por isso não foi criado a baixo das variáveis
+//O lombok esta criando os gatters, setters e o método construtor vazío e o completo, por isso não foi criado a baixo das variáveis
 @Getter
 @Setter
 @NoArgsConstructor
-public class PartidaGooleDTO implements Serializable {
+@AllArgsConstructor
+public class PartidaGoogleDTO implements Serializable {
 	
 	private String statusPartida;
 	private String tempoPartida;
--- src/main/java/br/com/exemplo/util/ScrapingUtil.java
+++ src/main/java/br/com/exemplo/util/ScrapingUtil.java
@@ -1,16 +1,62 @@
 package br.com.exemplo.util;
 
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import br.com.exemplo.dto.PartidaGoogleDTO;
+
 public class ScrapingUtil {
+	
+	
+	// Importado do org.slf4j
+	private static final Logger LOGGER = LoggerFactory.getLogger(ScrapingUtil.class);
 	
 	// ?q = query
 	private static final String BASE_URL_GOOGLE = "https://google.com.br/search?q=";
 	
-	// informa para o google qual a linguagem que estamos buscando
+	// Query que queremos buscar, ou seja, o parâmetro.
+	private static final String QUERY = "palmeiras+x+corinthias+08/08/2020";
+	
+	// informa para o google qual a linguagem que estamos buscando, também é um parâmtro.
 	private static final String COMPLEMENTO_URL_GOOGLE = "&hl=pt-BR"; 
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		String url = BASE_URL_GOOGLE + QUERY + COMPLEMENTO_URL_GOOGLE;
+		ScrapingUtil scraping = new ScrapingUtil();
+		
+		scraping.obtendoInformacoesPartida(url);
 
+	}
+	
+	public PartidaGoogleDTO obtendoInformacoesPartida(String utl) {
+		
+		PartidaGoogleDTO partida = new PartidaGoogleDTO();
+		
+		// Importado do org.jsoup.nodes
+		Document document = null;
+		
+		try {
+			
+			document = Jsoup.connect(utl).get();
+			
+			String title = document.title();
+			LOGGER.info("Titulo da página: {}", title);
+			
+		}
+		catch (IOException e) {
+			
+			LOGGER.error("Erro ao tentar conectar no google com JSoup -> {}", e.getMessage());
+			e.printStackTrace();
+			
+		}
+		
+		
+		return partida;
 	}
 
 }
