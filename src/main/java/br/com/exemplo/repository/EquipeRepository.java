c7fe03605fbf6212da8e9fbc911ba64b5a5c0508
andrlima <tc.leo.araujo@gmail.com>
2021-01-15 02:34:19 -0300

    GET, POST e PUT
    Desenvolvido os endponts Get, post e put.

--- src/main/java/br/com/exemplo/repository/EquipeRepository.java
+++ src/main/java/br/com/exemplo/repository/EquipeRepository.java
@@ -1,5 +1,7 @@
 package br.com.exemplo.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +11,9 @@ import br.com.exemplo.entity.Equipe;
 //Essa função faz o crud, porém ainda não entendi como realmente funcionar.
 @Repository
 public interface EquipeRepository extends JpaRepository<Equipe, Long> {
-
+	
+	//Recupera a equipe por nome e valida de a equipe existe pelo nome
+	public Optional<Equipe> findByNomeEquipe(String nomeEquipe);
+	
+	public boolean existsByNomeEquipe(String nomeEquipe);
 }
